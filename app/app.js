4465b2c83037e9cedf3df485d156d56d392fdfd9
mummum2 <chestnut0801@gmail.com>
2023-02-01 15:01:59 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -7,10 +7,10 @@ const app = express();
 const PORT = 3000;
 
 //라우팅 
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
